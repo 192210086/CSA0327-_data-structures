8b079eb1b06b813d969a50c7e62bab7f32a76bd2
192210086 <141109929+192210086@users.noreply.github.com>
2023-08-03 09:28:57 +0530

    Add files via upload

---
+++ linkedlistoperations.c
@@ -0,0 +1,33 @@
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+ 
+struct Node* push(struct Node* head, int data)
+{
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    
+	temp->data = data;
+    temp->next = head;
+    
+    return temp;
+} 
+int main()
+{
+	int i,n;
+    int values[] = {100, 200, 300, 400, 500};
+    n = sizeof(values)/sizeof(values[0]);
+
+    struct Node* head;
+    for (i = n - 1; i >= 0; i--) {
+        head = push(head, values[i]);
+    }
+ 
+    struct Node* ptr = head;
+    while (ptr)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
+}
